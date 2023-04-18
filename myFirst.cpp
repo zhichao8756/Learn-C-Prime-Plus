65d9c9667505a04caed05a579484fb933485e20e
zack1993 <8946147+zack1993@user.noreply.gitee.com>
2023-04-18 17:37:59 +0800

    feat: init

---
+++ myFirst.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main() {
+    using namespace std;
+    cout << "come up and c++ me somme time";
+    cout << endl;
+    cout << "you wont regret it"  << endl;
+    return 0;
+
+}
